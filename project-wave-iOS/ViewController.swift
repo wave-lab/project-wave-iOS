4e18529f293f69a2316e9c305f958b74229230c0
Seungsoo Lee <few280@gmail.com>
2019-07-01 16:46:06 +0900

    init project

---
+++ project-wave-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project-wave-iOS
+//
+//  Created by LeeSeungsoo on 01/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
