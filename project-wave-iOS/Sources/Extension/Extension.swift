58de1955dfb0db46193e95a0b261c2028a8959c5
Seungsoo Lee <few280@gmail.com>
2019-07-01 17:39:10 +0900

    docs: Set up default project file

---
+++ project-wave-iOS/Sources/Extension/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  project-wave-iOS
+//
+//  Created by LeeSeungsoo on 01/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import Foundation
---
+++ project-wave-iOS/Sources/Module/Splash/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashViewController.swift
+//  project-wave-iOS
+//
+//  Created by LeeSeungsoo on 01/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
