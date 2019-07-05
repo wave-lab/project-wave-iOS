ee193f44c26fd08524fca6dcc854566dc155e6e6
Seungsoo Lee <few280@gmail.com>
2019-07-05 20:59:24 +0900

    feat: Set up a collection of constants for easy use of resources.

---
+++ project-wave-iOS/Resources/Wave.swift
@@ -0,0 +1,55 @@
+//
+//  Wave.swift
+//  project-wave-iOS
+//
+//  Created by LeeSeungsoo on 05/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Wave {
+  struct reuseIdentifier {
+    static let menuCell = "MenuCell"
+  }
+  
+  struct storyboard {
+    static let splash = "Splash"
+    static let intro = "Intro"
+    static let sign = "Sign"
+    static let mainTab = "MainTab"
+    static let home = "Home"
+    static let assess = "Assess"
+    static let search = "Search"
+    static let library = "Library"
+    static let myPage = "MyPage"
+  }
+  
+  struct viewController {
+    static let splash = "SplashViewController"
+    static let intro = "IntroViewController"
+    static let home = "HomeViewController"
+    static let assess = "AssessViewController"
+    static let search = "SearchViewController"
+    static let library = "LibraryViewController"
+    static let myPage = "MyPageViewController"
+    
+  }
+  
+  struct color {
+   
+  }
+  
+  struct image {
+    
+  }
+  
+  struct font {
+    
+  }
+  
+  struct string {
+    
+  }
+}
