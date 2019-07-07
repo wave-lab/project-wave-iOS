970d8af4cb275699bcfc7d072ddfe65518deca7d
Seungsoo Lee <few280@gmail.com>
2019-07-07 22:28:39 +0900

    feat: Default set up horizontal song cell

--- project-wave-iOS/Sources/Common/Cells/HorizontalSongCell.swift
+++ project-wave-iOS/Sources/Common/Cells/HorizontalSongCell.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class HorizontalSongCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-      
+  
+  @IBOutlet weak var songImageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var artistNameLabel: UILabel!
+  @IBOutlet weak var genreLabel: UILabel!
+  
+  
+  var delegate: SongDetailDelegate?
+  
+  var item: String? {
+    didSet {
+      setupItems()
     }
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  @IBAction func showSongDetail(_ sender: Any) {
+    let index: Int = 0 //수정해야함
+    self.delegate?.showSongDetail(index: index)
+  }
+  
+}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-      
-    }
-    
+extension HorizontalSongCell {
+  func setupItems() {
+    //데이터 셋업 필요
+    titleLabel.sizeToFit()
+    artistNameLabel.sizeToFit()
+    genreLabel.sizeToFit()
+  }
 }
---
+++ project-wave-iOS/Sources/Common/Protocols/SongDetailDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SongDetailDelegate.swift
+//  project-wave-iOS
+//
+//  Created by LeeSeungsoo on 07/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import Foundation
+
+protocol SongDetailDelegate {
+  func showSongDetail(index: Int)
+}
