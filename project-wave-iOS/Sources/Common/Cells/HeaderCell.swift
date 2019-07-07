611819e6f155f0274ea86f4cf806bd7f0da1462d
yejin <dpwls7753@gmail.com>
2019-07-07 16:42:26 +0900

    feat: develop header cell

---
+++ project-wave-iOS/Sources/Common/Cells/HeaderCell.swift
@@ -0,0 +1,32 @@
+//
+//  HeaderCell.swift
+//  project-wave-iOS
+//
+//  Created by yejin on 07/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+  
+  @IBOutlet weak var headerLabel: UILabel!
+  
+  var item: String = [] {
+    didSet {
+      setItem()
+    }
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+  }
+  
+  func setItem() {
+    headerLabel.text = item
+    headerLabel.sizeToFit()
+  }
+  
+  
+}
