0d3b4de12069391c124d9b97dc422dbb36171ad1
yejin <dpwls7753@gmail.com>
2019-07-10 21:33:47 +0900

    feat: Develop playing view

---
+++ project-wave-iOS/Sources/Module/MainTab/PlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerView.swift
+//  project-wave-iOS
+//
+//  Created by yejin on 10/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
