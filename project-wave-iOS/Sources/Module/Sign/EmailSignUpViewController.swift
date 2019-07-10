06c0e3428aa17cc102a2078ad8222bd314e7168f
yejin <dpwls7753@gmail.com>
2019-07-10 18:42:52 +0900

    feat: Login and email sign up view images

---
+++ project-wave-iOS/Sources/Module/Sign/EmailSignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EmailSignUpViewController.swift
+//  project-wave-iOS
+//
+//  Created by yejin on 10/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import UIKit
+
+class EmailSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
