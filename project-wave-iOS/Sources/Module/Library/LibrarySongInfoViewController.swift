0e6bc02d98db1fe6a82b382de680352c080c9066
yejin <dpwls7753@gmail.com>
2019-07-11 16:22:08 +0900

    chore: Add images to library song info view

---
+++ project-wave-iOS/Sources/Module/Library/LibrarySongInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LibrarySongInfoViewController.swift
+//  project-wave-iOS
+//
+//  Created by yejin on 11/07/2019.
+//  Copyright © 2019 WAVE-Lab. All rights reserved.
+//
+
+import UIKit
+
+class LibrarySongInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
