7c33a8ade3c5a47abccdb358f43b80049d7e2e30
yejin <dpwls7753@gmail.com>
2019-07-11 20:49:24 +0900

    feat: Add library playlist information views

--- project-wave-iOS/Sources/Module/Library/LibrarySongInfoViewController.swift
+++ project-wave-iOS/Sources/Module/Library/LibrarySongInfoViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class LibrarySongInfoViewController: UIViewController {
 
-    override func viewDidLoad() {
+  
+  @IBOutlet weak var librarySongInfoLabel: UILabel!
+  @IBOutlet weak var coverArtistName: UILabel!
+  @IBOutlet weak var cancelBtn: UIButton!
+  
+  @IBAction func likeBtn(_ sender: Any) {
+  }
+  
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
+      
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
